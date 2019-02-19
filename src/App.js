27786a1cf57edfce879d2595737869efb2a300aa
daniel_w <danielwil@wp.pl>
2019-02-20 00:49:40 +0100

    redux-api-middleware demo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { connect } from 'react-redux';
+import { getData } from './actionCreators';
+
 class App extends Component {
+
+handleClick=()=>{
+  this.props.dispatch(getData());}
+
+showData=()=>{
+
+  return this.props.data.map(el=>(
+    <div key={el.name}>{el.name}</div>))
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <button onClick={this.handleClick}>GET</button>
+         {this.showData()}
+
+
         </header>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    data: state.data.results,
+  };
+}
+
+export default connect(mapStateToProps)(App);
